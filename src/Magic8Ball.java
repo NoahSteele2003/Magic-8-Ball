f8cb1828283048d61116f89e17c73bec7be8f502
Unknown <noah.steele.2021@student.lhschools.org>
2018-09-10 08:09:22 -0400

    Started project

---
+++ src/Magic8Ball.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[]args){
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        String question;
+        int value;
+
+        System.out.println("What is your question?");
+        question = keyboard.nextLine();
+        System.out.println("Pick a number between 1 and 100.");
+        value = keyboard.nextInt();
+
+    }
+}
