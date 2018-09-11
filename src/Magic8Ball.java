116babf8ebd0019eaac20d871e8117ccaede8f4b
Unknown <noah.steele.2021@student.lhschools.org>
2018-09-11 07:50:30 -0400

    Worked more on it

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -1,3 +1,6 @@
+import sun.awt.SunToolkit;
+import sun.java2d.pipe.LoopBasedPipe;
+
 import java.util.Scanner;
 
 public class Magic8Ball {
@@ -15,7 +18,18 @@ public class Magic8Ball {
         System.out.println("Pick a number between 1 and 100.");
         value = keyboard.nextInt();
 
-        if (value >= 1 && value<=10){ System.out.println("yes");}
+        if (value>=0 && value<=10){System.out.println("yes");}
+        else if (value>10 && value<=20){System.out.println("No");}
+        else if (value>20 && value<=30){System.out.println("The future looks promising");}
+        else if (value>30 && value<=40){System.out.println("All signs point to no");}
+        else if (value>40 && value<=50){System.out.println("It is guaranteed");}
+        else if (value>50 && value<=60){System.out.println("It is impossible");}
+        else if (value>60 && value<=70){System.out.println("Definitely yes");}
+        else if (value>70 && value<=80){System.out.println("It will never happen");}
+        else if (value>80 && value<=90){System.out.println("For sure!");}
+        else if (value>90 && value<=100){System.out.println("Definitely not");}
+
+
 
 
 
