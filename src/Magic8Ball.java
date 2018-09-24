8109428f5bdcea5cb3bd81a7ff44891a5f338bcf
Unknown <noah.steele.2021@student.lhschools.org>
2018-09-24 07:29:09 -0400

    Changed a little bit

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -9,13 +9,13 @@ public class Magic8Ball {
 
         String question;
         int value;
+        String response;
+        value = (int)(Math.random()*100);
 
         System.out.println("What is your question?");
         question = keyboard.nextLine();
-        System.out.println("Pick a number between 1 and 100.");
-        value = keyboard.nextInt();
 
-        if (value>=0 && value<=10){System.out.println("yes");}
+        if (value>=0 && value<=10){ System.out.println("yes");}
         else if (value>10 && value<=20){System.out.println("No");}
         else if (value>20 && value<=30){System.out.println("The future looks promising");}
         else if (value>30 && value<=40){System.out.println("All signs point to no");}
@@ -25,6 +25,38 @@ public class Magic8Ball {
         else if (value>70 && value<=80){System.out.println("It will never happen");}
         else if (value>80 && value<=90){System.out.println("For sure!");}
         else if (value>90 && value<=100){System.out.println("Definitely not");}
+
+
+        System.out.println("Would you like to ask another question?");
+        response = keyboard.next();
+
+        while(response.equals("yes")){
+            System.out.println("What is your question?");
+            question = keyboard.nextLine();
+            question = keyboard.nextLine();
+            value = (int)(Math.random()*100);
+
+            if (value>=0 && value<=10){ System.out.println("yes");}
+            else if (value>10 && value<=20){System.out.println("No");}
+            else if (value>20 && value<=30){System.out.println("The future looks promising");}
+            else if (value>30 && value<=40){System.out.println("All signs point to no");}
+            else if (value>40 && value<=50){System.out.println("It is guaranteed");}
+            else if (value>50 && value<=60){System.out.println("It is impossible");}
+            else if (value>60 && value<=70){System.out.println("Definitely yes");}
+            else if (value>70 && value<=80){System.out.println("It will never happen");}
+            else if (value>80 && value<=90){System.out.println("For sure!");}
+            else if (value>90 && value<=100){System.out.println("Definitely not");}
+
+            System.out.println("Would you like to ask another question?");
+            response=keyboard.next();
+        }
+
+        if(response.equals("no")){
+            System.out.println("Ok");
+        }
+
+
+
 
 
 
