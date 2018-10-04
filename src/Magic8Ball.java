05f16ba131dceddf48d45886ad82313dac95adbd
Unknown <noah.steele.2021@student.lhschools.org>
2018-10-04 07:37:08 -0400

    Made the code less redundant

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -11,13 +11,13 @@ public class Magic8Ball {
         int value;
         String response;
         response = "yes";
-        value = (int)(Math.random()*100);
+        /*value = (int)(Math.random()*100);
 
         System.out.println("What is your question?");
-        question = keyboard.nextLine();
-        int questions = 1;
+        question = keyboard.nextLine();*/
+        int questions = 0;
 
-        if (value>=0 && value<=10){ System.out.println("yes");}
+        /*if (value>=0 && value<=10){ System.out.println("yes");}
         else if (value>10 && value<=20){System.out.println("No");}
         else if (value>20 && value<=30){System.out.println("The future looks promising");}
         else if (value>30 && value<=40){System.out.println("All signs point to no");}
@@ -29,20 +29,18 @@ public class Magic8Ball {
         else if (value>90 && value<=100){System.out.println("Definitely not");}
 
         System.out.println("Would you like to ask another question?");
-        response = keyboard.nextLine();
+        response = keyboard.nextLine();*/
 
         while(response.equals("yes")){
+            value = (int)(Math.random()*100);
             questions++;
-            if(questions==2){
+            if(questions==1){
+                System.out.println("What is your question?");
+                question = keyboard.nextLine();}
+            if(questions>1){
                 System.out.println("What is your question?");
                 question = keyboard.nextLine();
-                value = (int)(Math.random()*100);}
-            if(questions>2){
-                System.out.println("What is your question?");
-                question = keyboard.nextLine();
-                question = keyboard.nextLine();
-                value = (int)(Math.random()*100);
-            }
+                question = keyboard.nextLine();}
 
 
             if (value>=0 && value<=10){ System.out.println("yes");}
